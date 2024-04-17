85db171ca76834979110bdfb494ad3785cc0a662
criDolby <166417409+criDolby@users.noreply.github.com>
2024-04-17 16:36:36 +0200

    Update cgt_conn_sfdc_p1.0.js

--- cgt_conn_sfdc_p1.0.js
+++ cgt_conn_sfdc_p1.0.js
@@ -46,7 +46,7 @@ async function initiateSSOFlow() {
     window.location = redirectURL;
 }
 
-function tokenExchange(response, codeVerifier, clientId, authorizeType, uniqueVisitorId) {
+function tokenExchange(response, codeVerifier) {
     sessionStorage.setItem("sorgente", sorgente);
     // Get Values from Code Response
     let code = response.code;
@@ -93,11 +93,11 @@ function tokenExchange(response, codeVerifier, clientId, authorizeType, uniqueVi
     }
 }
 
-function getUserInfo(accessToken, expDomain) {
+function getUserInfo(accessToken) {
     sessionStorage.setItem("sorgente", sorgente);
     userInfoURI = '/services/oauth2/userinfo';
     client = new XMLHttpRequest();
-    client.open("GET", expDomain + userInfoURI, true);
+    client.open("GET", commUrl + userInfoURI, true);
     client.setRequestHeader("Content-Type", "application/json");
     client.setRequestHeader("Authorization", 'Bearer ' + accessToken);
     client.send();
